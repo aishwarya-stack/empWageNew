35e6a73ab25c0ee6cc8a413d9c97fdcb4f93ea2e
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-18 15:50:48 +0530

    ADD:assuming 20 days per month

--- empWage.java
+++ empWage.java
@@ -4,13 +4,16 @@ public class empWage {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
-
+	public static final int NUM_OF_WORKING_DAYS=20;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		int empHr;
 		int empWage;
+		int totalEmpWage = 0;
 System.out.print("Welcome to Employee Wage Computation Program on Master Branch");
+for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+{
 int empCheck = (int) (Math.floor(Math.random()*10)%3);;
 // int rand_int2 = rand.nextInt(1000);
 
@@ -34,8 +37,10 @@ int empCheck = (int) (Math.floor(Math.random()*10)%3);;
 }
  
  empWage=empHr*EMP_RATE_PER_HOUR;
+ totalEmpWage+=empWage;
 System.out.println("emp wage"+empWage);
 	
 	}
-
+System.out.println("Total emp wage"+totalEmpWage);
+	}
 }
