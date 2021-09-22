802afd79823ef884badf9c7b9a7605cd60b602cc
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-22 11:15:30 +0530

    ADD:emp wage for each company calculated

--- Object.java
+++ Object.java
@@ -1,59 +1,84 @@
 package empWage;
 
-class Employee {
+import java.util.*;
+
+class Employee 
+{
 	// declaration of variables
 	int empCheck, empHr, empWage, totalEmpWage, totalWorkingDays, totalEmpHrs;
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
+	int NUM_OF_WORKING_DAYS;
+	int MAX_HRS_IN_MONTH;
 
-	
-	void CalculateWage() 
-	{
-		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-		System.out.println("" + empCheck);
+ 	     int numOfDays(int numOfWorkingDays)
+	    {
+		NUM_OF_WORKING_DAYS = numOfWorkingDays;
 
-		totalWorkingDays++;
+		return NUM_OF_WORKING_DAYS;
+       	}
 
-		switch (empCheck)
-		{
-		case 1:
-			empHr = 8;
-			System.out.print("full time");
-			break;
-		case 2:
-			empHr = 4;
-			System.out.print("part time");
-			break;
-		case 0:
-			empHr = 0;
-			System.out.println("absent");
-			break;
+	    int workingHrs(int maxHrInMonth)
+	   {
 
-		default:
-			empHr = 0;
-			break;
-		}
-		empWage = empHr * EMP_RATE_PER_HOUR;
-		totalEmpWage = totalEmpWage + empWage;
-		System.out.println("days" + totalWorkingDays + "emphrs" + empHr);
-		System.out.println("wage" + totalEmpWage);
-	}
+		MAX_HRS_IN_MONTH = maxHrInMonth;
 
+		return MAX_HRS_IN_MONTH;
+	   }
+
+	     int CalculateWage()
+    	{
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+	    	{
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+			System.out.println("" + empCheck);
+
+			totalWorkingDays++;
+
+			switch (empCheck) {
+			case 1:
+				empHr = 8;
+				System.out.print("full time");
+				break;
+			case 2:
+				empHr = 4;
+				System.out.print("part time");
+				break;
+			case 0:
+				empHr = 0;
+				System.out.println("absent");
+				break;
+
+			default:
+				empHr = 0;
+				break;
+			}
+			empWage = empHr * EMP_RATE_PER_HOUR;
+			totalEmpWage = totalEmpWage + empWage;
+			System.out.println("days" + totalWorkingDays + "emphrs" + empHr);
+			System.out.println("wage" + totalEmpWage);
+		   }
+		    return totalEmpWage;
+	    }
 }
 
-public class Object
+public class Object 
 {
-	public static void main(String args[])
+	public static void main(String args[]) 
 	{
-		Employee emp = new Employee();
+		Employee company = new Employee();
 		// int Check= (int) (Math.floor(Math.random()*10)%3);
 
-		for (int i = 0; i < 20; i++) {
-			emp.CalculateWage();
-		}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter Days:");
+		int Days = sc.nextInt();
+		System.out.println("enter Hrs:");
+		int Hrs = sc.nextInt();
+		company.numOfDays(Days);
+		company.workingHrs(Hrs);
+		company.CalculateWage();
 
 	}
+
 }
